ef4518d34c3652a253f2b149623e2ebbf19e9180
AuthorChe | Vadym_Yem <81570039+VadymYem@users.noreply.github.com>
2023-03-03 15:39:48 +0200

    Update skript.js

--- skript/skript.js
+++ skript/skript.js
@@ -3,14 +3,14 @@
     if($("#type_text").length){
         let text1 = new TypeIt("#type_text", {
             speed: 70,
-            strings: ["Привiт, вiтаю вас тут. Це мiй сайт-вiзитка. Вiн поки що на стадii розробки :)"],
+            strings: ["Привiт, вiтаю вас тут. Це мiй сайт. Вiн поки що на стадii розробки :)"],
         }).go();
     }
     
     if($("#simpleUsage").length){
         let text2 = new TypeIt("#simpleUsage", {
             speed: 50,
-            strings: ["Ви серйозно?», «Ви вже на GitHub :)"],
+            strings: ["Ти серйозно?», «Ми вже тут :)"],
         }).go();
     }
 });
