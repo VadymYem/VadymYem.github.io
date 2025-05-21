30865a992c14fb5d051c71ab9ce60e6fb701dde9
AuthorChe | Vadym_Yem <81570039+VadymYem@users.noreply.github.com>
2025-05-21 16:40:08 +0300

    Create items.js

---
+++ items.js
@@ -0,0 +1,20 @@
+const items = [
+  {
+    name: "Загадковий Артефакт X",
+    description: "Цей предмет прибув з далеких світів, огортаний таємницею...",
+    price: 1200,
+    image: "https://authorche.pp.ua/poems/logo.jpg "
+  },
+  {
+    name: "Еліксир Натхнення",
+    description: "Магічний напій, що відкриває нові горизонти творчості...",
+    price: 550,
+    image: "https://authorche.pp.ua/poems/logo.jpg "
+  },
+  {
+    name: "Амулет Успіху",
+    description: "Носіть цей амулет, щоб привабити удачу...",
+    price: 890,
+    image: "https://authorche.pp.ua/poems/logo.jpg "
+  }
+];
